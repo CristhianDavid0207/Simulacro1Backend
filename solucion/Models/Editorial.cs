8a25ed4b6038c48f0327f2b469d896b66b2b88e4
CristhianDavid0207 <emmacris04@gmail.com>
2024-05-29 21:07:40 -0500

    feat: Completandoo Models/Book.cs y Editorial.cs

--- solucion/Models/Editorial.cs
+++ solucion/Models/Editorial.cs
@@ -1,21 +1,31 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace solucion.Models
 {
     public class Editorial
     {
-        public int Id {get; set;}
-        public string? Name {get; set;}
-        public string? Address {get; set;}
-        public string? Phone {get; set;}
-        // public Status Status { get; set; } 
+        [Required(ErrorMessage = "El Id es obligatorio.")]
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "El Nombre es obligatorio."),
+        MaxLength(125, ErrorMessage = "El Nombre no puede tener más de 125 caracteres.")]
+        public string? Name { get; set; }
 
-        // public class enum Status {Active, Inactive}
+        [Required(ErrorMessage = "La Dirección es obligatoria."),
+        MaxLength(125, ErrorMessage = "La Dirección no puede tener más de 125 caracteres.")]
+        public string? Address { get; set; }
+
+        [Required(ErrorMessage = "El Teléfono es obligatorio.")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "El Estado es obligatorio.")]
+        public string? Status { get; set; }
 
         //Relación 1:N
         [JsonIgnore]
-        public List<Book>? Books {get; set;}
+        public List<Book>? Books { get; set; }
 
     }
 }
+
